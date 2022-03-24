119d07ba5d060800476418b56caac0f085f1465d
calmiyster <callum12345678@hotmail.com>
2022-03-24 09:33:05 -0700

    gallery change

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -3,19 +3,19 @@
  */
 import React from 'react';
 import ImageSpinner from './ImageSpinner';
-import slide1 from '../images/gallery/slide-1-min.png' ;
-import slide2 from '../images/gallery/newslide2-min.png' ;
-import slide3 from '../images/frontage_1-min.jpg' ;
+import slide1 from '../images/gallery/New Banner Compilation.png' ;
+import slide2 from '../images/gallery/food pics compilation.png' ;
+import slide3 from '../images/gallery/Sunshine_21.jpg' ;
 import slide4 from '../images/gallery/newslide1-min.png' ;
-import slide5 from '../images/gallery/newslide4-min.png' ;
-import slide6 from '../images/gallery/newslide3-min.png' ;
-import slide7 from '../images/gallery/newslide5-min.png' ;  
+import slide5 from '../images/gallery/newslide5-min.png' ;
+import slide6 from '../images/gallery/newslide2-min.png' ;
+import slide7 from '../images/gallery/frontage_1_crop-min.jpg.png' ;  
 import slide8 from '../images/gallery/newslide6-min.png' ;
 import slide9 from '../images/gallery/slide-9-min.png' ;
 import slide11 from '../images/gallery/slide-11-min.png' ;
 
 const Gallery = () => {
-  let images = [slide1, slide2, slide3, slide4, slide5, slide6, slide7, slide8, slide9, slide11];
+  let images = [slide1, slide2, slide3, slide4, slide5, slide6, slide7];
   return (
     <div>
       <h2 className="major">Gallery</h2>
