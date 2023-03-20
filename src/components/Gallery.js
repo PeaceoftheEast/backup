6246af8a4398aaaf053b54bbd4cbee1043f0f423
Wip <wip.abramson@gmail.com>
2023-03-20 20:14:42 +0000

    update gallery with new pics

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -3,18 +3,20 @@
  */
 import React from 'react';
 import ImageSpinner from './ImageSpinner';
-import slide1 from '../images/gallery/New Banner Compilation.png' ;
-import slide2 from '../images/gallery/food pics compilation.png' ;
-import slide3 from '../images/gallery/Sunshine_21.jpg' ;
-import slide4 from '../images/gallery/newslide1-min.png' ;
-import slide5 from '../images/gallery/newslide5-min.png' ;
-import slide6 from '../images/gallery/newslide2-min.png' ;
-import slide7 from '../images/gallery/frontage_1_crop-min.jpg' ;  
-import slide8 from '../images/gallery/slide-9-min.png' ;
-import slide9 from '../images/gallery/REDUCED_FILE_SIZE_Festival_Menu_.jpg' ;
+import slide1 from '../images/gallery/Gallery01.jpg' ;
+import slide2 from '../images/gallery/Gallery02.jpg' ;
+import slide3 from '../images/gallery/Gallery03.jpg' ;
+import slide4 from '../images/gallery/Gallery04.jpg' ;
+import slide5 from '../images/gallery/Gallery05.jpg' ;
+import slide6 from '../images/gallery/Gallery06.jpg' ;
+import slide7 from '../images/gallery/Gallery07.jpg' ;  
+import slide8 from '../images/gallery/Gallery08.png' ;
+import slide9 from '../images/gallery/Gallery09.jpg' ;
+import slide10 from '../images/gallery/Gallery10.jpg' ;
+import slide11 from '../images/gallery/Gallery11.jpg' ;
 
 const Gallery = () => {
-  let images = [slide1, slide2, slide3, slide4, slide5, slide6, slide7, slide8, slide9];
+  let images = [slide1, slide2, slide3, slide4, slide5, slide6, slide7, slide8, slide9, slide10, slide11];
   return (
     <div>
       <h2 className="major">Gallery</h2>
